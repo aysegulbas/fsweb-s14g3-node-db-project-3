7cdaf918c035e7acd7d4c313f9732edb489622d7
Aysegul BAS <aysegul.bas@arkas.com>
2023-06-22 00:31:24 +0300

    ok

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const schemeModel = require("./scheme-model");
 /*
   Eğer `scheme_id` veritabanında yoksa:
 
@@ -6,9 +7,22 @@
     "message": "scheme_id <gerçek id> id li şema bulunamadı"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
-}
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const isexistedScheme = await schemeModel.findById(req.params.scheme_id);
+    if (!isexistedScheme) {
+      //!null true olayını çalıştırdık, model de null tanımlamazsak 500 hatasına düşcek
+      res.status(404).json({
+        message: `scheme_id ${req.params.scheme_id} id li şema bulunamadı`,
+      });
+    } else {
+      req.existedScheme = isexistedScheme;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 /*
   Eğer `scheme_name` yoksa, boş string ya da string değil:
@@ -19,8 +33,18 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  try {
+    const schemeName = req.body.scheme_name;
+    if (!schemeName || typeof schemeName !== "string") {
+      //boş string falsy olduğu için boş string şartı koymadık
+      res.status(400).json({ message: "Geçersiz scheme_name" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 /*
   Eğer `instructions` yoksa, boş string yada string değilse, ya da
@@ -32,11 +56,25 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
-}
+  try {
+    const { instructions, step_number } = req.body;
+    if (
+      !instructions ||
+      typeof instructions !== "string" ||
+      typeof step_number !== "number" ||
+      step_number < 1
+    ) {
+      res.status(400).json({ message: "Hatalı step" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,11 +1,17 @@
-function find() { // Egzersiz A
+/**
+ * @param { import("knex").Knex } db
+ * @returns { Promise<void> }
+ */
+const db = require("../../data/db-config");
+function find() {
+  // Egzersiz A
   /*
     1A- Aşağıdaki SQL sorgusunu SQLite Studio'da "data/schemes.db3" ile karşılaştırarak inceleyin.
-    LEFT joini Inner joine çevirirsek ne olur?
+    LEFT joini Inner joine çevirirsek ne olur? NUMBER_OF_STEPS 0 olan 7 idli datayı da ekler
 
       SELECT
           sc.*,
-          count(st.step_id) as number_of_steps
+          count(st.step_idst.step_id) as number_of_steps
       FROM schemes as sc
       LEFT JOIN steps as st
           ON sc.scheme_id = st.scheme_id
@@ -14,10 +20,18 @@ function find() { // Egzersiz A
 
     2A- Sorguyu kavradığınızda devam edin ve onu Knex'te oluşturun.
     Bu işlevden elde edilen veri kümesini döndürün.
+    scheme_id-scheme_name_number_of_steps
   */
+  return db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+    .select("sc.*")
+    .count("st.step_id as number_of_steps")
+    .groupBy("sc.scheme_id")
+    .orderBy("sc.scheme_id", "asc");
 }
 
-function findById(scheme_id) { // Egzersiz B
+async function findById(scheme_id) {
+  // Egzersiz B
   /*
     1B- Aşağıdaki SQL sorgusunu SQLite Studio'da "data/schemes.db3" ile karşılaştırarak inceleyin:
 
@@ -82,10 +96,61 @@ function findById(scheme_id) { // Egzersiz B
         "scheme_name": "Have Fun!",
         "steps": []
       }
+      
   */
-}
+  const schemeTable = await db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+    .select("sc.scheme_name", "st.*")
+    .where("sc.scheme_id", scheme_id)
+    .orderBy("st.step_number", "asc");
 
-function findSteps(scheme_id) { // Egzersiz C
+  if (schemeTable.length == 0) {
+    return null;
+  }
+  let responseData = {
+    scheme_id: Number(scheme_id),
+    scheme_name: schemeTable[0].scheme_name,
+    steps: [],
+  };
+  //schemeTable array dönüyor. eğer uzunluğu 0 ise null, değilse bize array gelecek
+  //gelen arrayda ilk elemanın ilk satırı name ve id numarası verip çağırdığımızda
+  //hepsinin ismi ve scheme id aynı gelir [{},{},{}]
+  //o yüzden schemeTable[0].scheme_name şeklinde yazdık. null olmadan gelmesi için en az bir elemanlı olmalı bu yüzden 0 dedik
+  if (!schemeTable[0].step_id) {
+    return responseData;
+  }
+  for (let i = 0; i < schemeTable.length; i++) {
+    const item = schemeTable[i];
+    let stepModel = {
+      step_id: item.step_id,
+      step_number: item.step_number,
+      instructions: item.instructions,
+    };
+    responseData.steps.push(stepModel);
+  }
+  return responseData;
+}
+//yeni scheme=>
+//step yok, scheme ekle step[]
+//step var scheme ve step ekle
+//eski scheme=>
+//step yok, step[]
+//steo var,eski scheme'e  step ekle
+//   const result = schemeTable.reduce((acc, item) => {
+//     const registeredScheme = acc.find(
+//       (scheme) => scheme.scheme_id === item.scheme_id
+//     );
+//   }, []);
+// }
+
+function findSteps(scheme_id) {
+  const steps = db("steps as st")
+    .leftJoin("schemes as sc", "st.scheme_id", "sc.scheme_id")
+    .select("st.step_id", "st.step_number", "st.instructions", "sc.scheme_name")
+    .where("sc.scheme_id", scheme_id)
+    .orderBy("st.step_number", "asc");
+  return steps;
+  // Egzersiz C
   /*
     1C- Knex'te aşağıdaki verileri döndüren bir sorgu oluşturun.
     Adımlar, adım_numarası'na göre sıralanmalıdır ve dizi
@@ -108,13 +173,20 @@ function findSteps(scheme_id) { // Egzersiz C
   */
 }
 
-function add(scheme) { // Egzersiz D
+async function add(scheme) {
+  let [id] = await db("schemes").insert(scheme);
+  return findById(id);
+  // Egzersiz D
   /*
     1D- Bu işlev yeni bir şema oluşturur ve _yeni oluşturulan şemaya çözümlenir.
   */
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+async function addStep(scheme_id, step) {
+  step.scheme_id = scheme_id;
+  await db("steps").insert(step);
+  return findSteps(scheme_id);
+  // EXERCISE E
   /*
     1E- Bu işlev, verilen 'scheme_id' ile şemaya bir adım ekler.
     ve verilen "scheme_id"ye ait _tüm adımları_ çözer,
@@ -128,4 +200,4 @@ module.exports = {
   findSteps,
   add,
   addStep,
-}
+};
